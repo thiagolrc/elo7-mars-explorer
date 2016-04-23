2dbc82d3a19fea3bbf7a1548ef0e29aa4fb180b5
thiago <thiago.lourenconi@buscapecompany.com>
2016-04-23 18:37:55 -0300

    some javadoc for Probe

--- mars-explorer/src/main/java/com/elo7/marsexplorer/Probe.java
+++ mars-explorer/src/main/java/com/elo7/marsexplorer/Probe.java
@@ -13,18 +13,34 @@ class Probe {
 		this.plateau = plateau;
 	}
 
-	Position executeCommand(final NavigationCommand navigationCommand) {
+	/**
+	 * Executa um {@link NavigationCommand} e altera a posição da sonda conforme
+	 * o comando
+	 * 
+	 * @return {@link Position} representando a FS nova localização da câmera
+	 * @throws IllegalArgumentException
+	 *             caso o comando tente navegar a sonda para fora do planalto de
+	 *             exploração a que ela pertence.
+	 */
+	Position executeCommand(final NavigationCommand navigationCommand) throws IllegalArgumentException {
 		Position newPosition = navigationCommand.determineNewPosition(position);
 		plateau.validatePositioning(newPosition);
 		this.position = newPosition;
 		return position;
 	}
 
+	/**
+	 * Executa um conjunto de {@link NavigationCommand}, recalculando, validando
+	 * e alterando a posição da sonda a cada comando.
+	 */
 	Position executeCommands(final List<NavigationCommand> navigationCommands) {
 		navigationCommands.stream().forEach(c -> this.executeCommand(c));
 		return position;
 	}
 
+	/**
+	 * @return {@link Position} atual da sonda.
+	 */
 	Position getPosition() {
 		return position;
 	}
